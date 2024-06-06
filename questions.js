381d0b9cc1e4c0aac940cce85cd52bfa302809db
Fernanda Martins <141344853+nandinhafer@users.noreply.github.com>
2024-06-06 12:22:22 -0300

    subindo arquivos iniciais
    quiz agro teste

---
+++ questions.js
@@ -0,0 +1,101 @@
+let questions = [
+   
+   
+    {
+        question: 'Qual é o maior produtor de arroz nacionalmente?',
+        options: [
+            'Rio Grande do Sul',//0
+            'China',//1
+            'Índia',//2
+            'Tailândia'//3
+        ],
+        answer: 0
+    },
+    
+   
+    {
+        question: 'Um exemplo de um processo histórico-econômico que modernizou a atividade agrícola, especialmente a partir da segunda metade do século XX, foi a chamada:',
+        options: [
+            'Primavera Árabe.', //0
+            'Revolução Verde.',//1
+            'Reforma agrária.',//2
+            'Globalização.'//3
+        ],
+        answer: 1
+    },
+
+   
+    {
+        question: 'As alternativas abaixo apresentam características da agricultura brasileira, com exceção da:',
+        options: [
+            'Diminuta produtividade por hectare.', //0
+            'Produção voltada para a exportação.',//1
+            'Ampla utilização de agroquímicos.',//2
+            'Prática da monocultura de espécies.'//3
+        ],
+        answer: 0
+    },
+
+    {
+        question: 'Qual o tipo de agricultura, amplamente praticado no Brasil a partir da segunda metade do século XX, que está atrelado à Revolução Verde?',
+        options: [
+            'Agricultura extensiva.', //0
+            'Agricultura familiar.',//1
+            'Agricultura moderna.',//2
+            'Agricultura orgânica.'//3
+        ],
+        answer: 2   
+    },
+
+   
+    {
+        question: 'As alternativas abaixo apresentam características da chamada agricultura familiar, ainda presente no interior brasileiro, com exceção da:',
+        options: [
+            'Adoção de mão de obra muito qualificada.',
+            'Diminuição do emprego de agroquímicos.',
+            'Cultivação de bens para o mercado interno.',
+            'Utilização de técnicas tradicionais de cultivo.'
+
+        ],
+        answer: 0  
+    },
+
+    {
+        question: 'Assinale a alternativa que apresenta uma consequência do processo de modernização da agricultura brasileira:',
+        options: [
+         'Diminuição da produtividade das lavouras.',
+         'Atenuação do êxodo rural do interior local.',
+         'Redução do desemprego do tipo estrutural.',
+         'Modificação intensa das paisagens naturais.'
+
+        ],
+        answer: 3
+    },
+
+
+    {
+        question: 'As alternativas abaixo apresentam os principais produtos vinculados à produção agrícola brasileira. NÃO é um desses produtos:',
+        options: [
+         'Trigo.',
+         'Soja.',
+         'Milho.',
+         'Algodão.'
+
+        ],
+        answer: 0
+    },
+
+
+    {
+        question: 'São impactos ambientais significativos no espaço rural brasileiro, com exceção da:',
+        options: [
+            'Poluição das fontes de água.',
+            'Manutenção da vegetação',
+            'Contaminação das partes do solo',
+            'Erosão das formas de relevo'
+
+        ],
+        answer: 1
+    },
+
+];
